a72bf8038cc4ad78661095290c28282697b7c937
ELLY-KELVIN <ELLYKELVIN150@GMAIL.COM>
2022-02-21 12:00:18 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { ReopsitoriesComponent } from './reopsitories/reopsitories.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { UserserviceService } from './userservice.service';
+import { HighlightDirective } from './highlight.directive';
+import { DatePipePipe } from './date-pipe.pipe';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NavbarComponent,
+    SearchFormComponent,
+    ReopsitoriesComponent,
+    NotfoundComponent,
+    HighlightDirective,
+    DatePipePipe,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
-  providers: [],
+  providers: [UserserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
