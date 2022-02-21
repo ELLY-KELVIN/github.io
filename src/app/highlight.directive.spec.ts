5b389f6f319dbc814b11f10dd296c6dc1be3fb2e
ELLY-KELVIN <ELLYKELVIN150@GMAIL.COM>
2022-02-21 12:01:02 +0300

    commit

---
+++ src/app/highlight.directive.spec.ts
@@ -0,0 +1,12 @@
+import { HighlightDirective } from './highlight.directive';
+
+describe('HighlightDirective', () => {
+  it('should create an instance', () => {
+    // const directive = new HighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+function directive(directive: any) {
+  throw new Error('Function not implemented.');
+}
+
